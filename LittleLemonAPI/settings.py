8402183b7fdf9bd64da8c1f2b65f55eecd40f661
James Murigi <j.murigi.mungai@gmail.com>
2026-01-24 13:04:50 +0300

    added whitenoise

--- LittleLemonAPI/settings.py
+++ LittleLemonAPI/settings.py
@@ -128,8 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Optional: compress and cache static files
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
