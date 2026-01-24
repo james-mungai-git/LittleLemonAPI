a5da1ffc10ea0630d67cd60f7a59d5531d2352fd
James Murigi <j.murigi.mungai@gmail.com>
2026-01-24 09:42:22 +0300

    added static

--- LittleLemonAPI/settings.py
+++ LittleLemonAPI/settings.py
@@ -89,10 +89,6 @@ else:
     }
 
 
-
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -128,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
