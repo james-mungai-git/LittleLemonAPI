8ff311d30ae71c79a9d68ab404045dc40a40d5ab
James Murigi <j.murigi.mungai@gmail.com>
2026-01-24 13:40:15 +0300

    made changes

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
-from django.urls import path,include
-from accounts import views
+from django.urls import path, include
+from accounts import views 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
     path('', views.landing, name='landing'),
+
     path('admin/', admin.site.urls),
-    path('api/',include('LittleLemon.urls')),
-    path('api/',include('accounts.urls')),
-    path('api/',include('menu.urls')),
+
+    path('api/', include('LittleLemon.urls')),
+    path('api/', include('accounts.urls')),
+    path('api/', include('menu.urls')),
+
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
