72ba04f4ea3562ee4f8850330d9def5d55af8550
James Murigi <j.murigi.mungai@gmail.com>
2026-01-24 13:58:35 +0300

    bluuh

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts import views 
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from drf_spectacular.views import SpectacularRedocView
-
+from accounts import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/docs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc-ui'),
 
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc-ui'),
 
     path('', views.landing, name='landing'),
 
